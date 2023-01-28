569897fc87d85be63b94dcdea4b43823ea583a1a
aless133 <aless133@yandex.ru>
2023-01-28 16:19:33 +0300

    feat: remove ProfilesAPI.getMemberType

--- src/datasources/profiles.rest.ts
+++ src/datasources/profiles.rest.ts
@@ -1,6 +1,6 @@
 import { CommonAPI } from './common.rest';
 import type { ProfileEntity } from '../utils/DB/entities/DBProfiles';
-import type { MemberTypeEntity } from '../utils/DB/entities/DBMemberTypes';
+// import type { MemberTypeEntity } from '../utils/DB/entities/DBMemberTypes';
 
 export class ProfilesAPI extends CommonAPI {
   async getProfiles(): Promise<ProfileEntity[]> {
@@ -11,7 +11,4 @@ export class ProfilesAPI extends CommonAPI {
     return this.get<ProfileEntity>(`profiles/${encodeURIComponent(id)}`);
   }
 
-  async getMemberType(id: string): Promise<MemberTypeEntity> {
-    return this.get<MemberTypeEntity>(`profiles/${encodeURIComponent(id)}/member-type`);
-  }
 }
