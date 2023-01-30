1b88d2e5ee16d86264461519b1522fa8a11c449f
aless133 <aless133@yandex.ru>
2023-01-30 09:47:29 +0300

    test: comment sample-data

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import { join } from 'path';
 import AutoLoad from '@fastify/autoload';
 import { FastifyPluginAsync } from 'fastify';
-import { addSampleData } from './sample-data';
+//import { addSampleData } from './sample-data';
 
 const app: FastifyPluginAsync = async (fastify): Promise<void> => {
   await fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
     options: {},
   });
-  await addSampleData(fastify.db);
+  //await addSampleData(fastify.db);
 
   fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
